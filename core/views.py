f1b9460809fe11a86b23c5f19221f77924ead837
Melany N. Monroy <mmonroy@ase.corp>
2024-08-13 16:38:52 -0500

    update

--- core/views.py
+++ core/views.py
@@ -6,9 +6,21 @@ from rest_framework import generics
 from core.serializers import FilesSerializer
 from core.models import Files
 
+import google.generativeai as genai
+import pathlib
+
 class ChatbotView(viewsets.ModelViewSet):
     queryset = Files.objects.all()
     serializer_class = FilesSerializer
 
+    # Log in to huggingface and grant authorization to huggingchat
+    
+
     def list(self, request):
-        return Response({"test":"this is a test"})
+        model = genai.GenerativeModel("gemini-1.5-flash")
+        response = model.generate_content("Write a story about a magic backpack.")
+
+        return Response({"response":response.text})
+
+
+        
