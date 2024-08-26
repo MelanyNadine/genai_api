1d050d6f3912e66baa335edec39206f3c82df59e
Melany N. Monroy <mmonroy@ase.corp>
2024-08-26 11:03:27 -0500

    cleaning up the mess...

--- genai_api/settings.py
+++ genai_api/settings.py
@@ -11,7 +11,10 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
 import os
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -22,7 +25,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'files')
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-kx7+#o6-cwg41t3efv#+8aa4xonycba&pt=fm2onx(8ldm1!^*"
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
